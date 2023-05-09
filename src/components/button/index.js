b363429a498ff7e86e6f91db0fc498750aff2d55
KevinZhou <1065438834@qq.com>
2023-05-09 16:27:19 +0800

    加上typescript

--- src/components/button/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./styles.css";
-
-function KButton({ type }) {
-  const btnClass = `k-btn k-btn-${type ? type : "primary"}`;
-  return <button className={btnClass}>按钮</button>;
-}
-
-export default KButton;
