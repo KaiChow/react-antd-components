e1ea434a23fcfd7adc64d2ee045da34eb037f3ba
KevinZhou <1065438834@qq.com>
2023-05-09 17:10:16 +0800

    普通的按钮

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,29 +1,7 @@
 import React from "react";
-const ButtonTypes = [
-  "default",
-  "primary",
-  "ghost",
-  "dashed",
-  "link",
-  "text",
-] as const;
-/**
- * as const
- * ts 修饰符 表示不可变
- */
-export type ButtonType = (typeof ButtonTypes)[number];
+import { BtnPropsTypes } from "./types";
 
-const ButtonShapes = ["default", "circle", "round"] as const;
-export type ButtonShape = (typeof ButtonShapes)[number];
-
-const ButtonHTMLTypes = ["submit", "button", "reset"] as const;
-export type ButtonHTMLType = (typeof ButtonHTMLTypes)[number];
-
-interface PropsTypes {
-  type?: ButtonType;
-  block?: boolean;
-}
-function KButton(props: PropsTypes) {
+function KButton(props: BtnPropsTypes) {
   const { type = "primary", block = false } = props;
   const classNames = `k-btn k-btn-${type} ${block ? "k-btn-block" : null}`;
   return <button className={classNames}>按钮</button>;
---
+++ src/components/button/types.ts
@@ -0,0 +1,24 @@
+const ButtonTypes = [
+  "default",
+  "primary",
+  "ghost",
+  "dashed",
+  "link",
+  "text",
+] as const;
+/**
+ * as const
+ * ts 修饰符 表示不可变
+ */
+export type ButtonType = (typeof ButtonTypes)[number];
+
+const ButtonShapes = ["default", "circle", "round"] as const;
+export type ButtonShape = (typeof ButtonShapes)[number];
+
+const ButtonHTMLTypes = ["submit", "button", "reset"] as const;
+export type ButtonHTMLType = (typeof ButtonHTMLTypes)[number];
+
+export type BtnPropsTypes = {
+  type?: ButtonType;
+  block?: boolean;
+};
